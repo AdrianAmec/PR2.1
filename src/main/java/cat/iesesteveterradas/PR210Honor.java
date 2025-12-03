d61eae9d927ab7e4a132fad3b80afe7c42e47c1f
AdrianAmec <adriancmec@gmail.com>
2025-12-03 15:30:37 +0100

    v

---
+++ src/main/java/cat/iesesteveterradas/PR210Honor.java
@@ -0,0 +1,12 @@
+package cat.iesesteveterradas;
+
+public class PR210Honor {
+    
+    
+    public static void main(String[] args) {
+
+        String basePath = System.getProperty("user.dir") + "/data/";
+        String filePath = basePath + "database.db";
+        
+    }
+}
